10d6c8d14f81e942757ae5b66ed42a55b55119ad
Zubair Shaheen <90441793+R-ZS-Dev@users.noreply.github.com>
2021-09-10 14:22:10 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include "include/db.php" ?>
+<?php include 'include/session.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "include/meta.php" ?>
+	<?php include "include/css.php" ?>
+	<?php include "include/title.php" ?>
+</head>
+<body>
+	<?php include "include/navbar.php" ?>
+	<?php include "include/slider.php" ?>
+	<?php include "include/text.php" ?>
+	<?php include "include/first.php" ?>
+	<?php include 'include/imghover.php'; ?>
+	<?php include 'include/secondtext.php'; ?>
+	<?php include 'include/slider2.php'; ?>
+	<?php include 'include/threetext.php'; ?>
+	<?php include 'include/insta.php'; ?>
+	<?php include 'include/footer.php'; ?>
+	<?php include "include/js.php" ?>
+</body>
+</html>
